fc07d9849c0319f7fdc37c3f399d7324527f6cb9
nickniu1 <448562031@qq.com>
2022-09-03 18:15:30 +0800

    update

--- mgmt/settings.py
+++ mgmt/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mgmt.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.sqlite3',
          'NAME': BASE_DIR / 'db.sqlite3',
